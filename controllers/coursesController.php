ee45d7f642a05aacc43c1790f9ae547957b97dec
aichaellyadri60-debug <aichaelyadri60@gmail.com>
2025-12-09 09:10:54 +0100

    la partie controller de create course

--- controllers/coursesController.php
+++ controllers/coursesController.php
@@ -6,20 +6,16 @@ if ($action == "list") {
 }
 
 if ($action == "store" && $_SERVER['REQUEST_METHOD'] === 'POST') {
-
     $title = $_POST["title"];
     $description = $_POST["description"];
     $level = $_POST["level"];
     if (!empty($_FILES['imagecourses']['name'])) {
-
         $imageName = time() . "_" . basename($_FILES['imagecourses']['name']);
         $targetDir = "./images/";
 
         move_uploaded_file($_FILES["imagecourses"]["tmp_name"], $targetDir . $imageName);
     }
-
     stockercourses($title, $description, $level, $imageName);
-
     header("Location: index.php?page=courses&action=list");
     exit;
 }
